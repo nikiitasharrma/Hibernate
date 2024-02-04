726b0653eab5a9b427da589184e6cadc837f6536
nikiitasharrma <nikita16301@gmail.com>
2024-02-04 13:16:24 +0530

    Configured hibernate

---
+++ HibernateCfg/src/main/java/com/hibernate/HibernateCfg/App.java
@@ -0,0 +1,14 @@
+package com.hibernate.HibernateCfg;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SessionFactory factory = new Configuration().configure().buildSessionFactory();
+    	
+    	System.out.println(factory);
+    }
+}
